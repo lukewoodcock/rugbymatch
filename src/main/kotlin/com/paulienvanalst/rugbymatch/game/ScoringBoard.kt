fd8ef128adba74f056c9d257bf8cc6dd6c8d7e87
lukewoodcock <lukewoodcock@gmail.com>
2019-04-18 14:12:57 +0200

    Exercise 3 completed BUT with 2 failing tests. However to think Toulon will beat WASPS by 5 points is unreaslistic ;)

--- src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
+++ src/main/kotlin/com/paulienvanalst/rugbymatch/game/ScoringBoard.kt
@@ -1,16 +1,30 @@
 package com.paulienvanalst.rugbymatch.game
 
+import com.paulienvanalst.rugbymatch.events.ScoringEvent
+import com.paulienvanalst.rugbymatch.events.StartGame
 import com.paulienvanalst.rugbymatch.team.NotImplementedException
+import com.paulienvanalst.rugbymatch.team.TeamName
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
 open class ScoringBoard {
+    private lateinit var hostingTeam : TeamName
+    private lateinit var visitingTeam : TeamName
 
     private var scoringHistory : List<Score> = emptyList()
 
-    fun currentScore(): GameScore {
-        throw NotImplementedException()
+    @EventListener
+    fun initialize(start: StartGame) {
+        hostingTeam = start.hostingTeam
+        visitingTeam = start.visitingTeam
     }
+
+    fun onScoringEvent(scoreEvent: ScoringEvent) {
+        scoringHistory += Score(scoreEvent.team, scoreEvent.type)
+    }
+
+    fun currentScore(): GameScore = scoringHistory.getGameScore(hostingTeam, visitingTeam)
 
     fun clear () {
         scoringHistory = emptyList()
